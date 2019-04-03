4753462045182cdc6061a7f3d9a3afb8941f649e
shicks <shicks255@yahoo.com>
2019-04-03 15:47:07 -0400

    adding the gif

--- src/components/NowPlaying.jsx
+++ src/components/NowPlaying.jsx
@@ -12,23 +12,23 @@ export default class NowPlaying extends React.Component {
         let content
         if (this.props.nowPlaying)
             content =
-                <div className={"level-item"} >
-                    <img className={"image"} src={this.props.nowPlaying.image[0]['#text']}/> {this.props.nowPlaying.artist['#text']} - {this.props.nowPlaying.name}
+                <div>
+                    <div className={"level"} style={{marginBottom: "0"}}>
+                        <div className={"level-item"}>
+                            Now Playing...
+                            <img className={"gif"} src={process.env.PUBLIC_URL + "/YdBO.gif"}/>
+                        </div>
+                    </div>
+                    <div className={"level"}>
+                        <div className={"level-item"} >
+                            <img className={"image"} src={this.props.nowPlaying.image[0]['#text']}/> {this.props.nowPlaying.artist['#text']} - {this.props.nowPlaying.name}
+                        </div>
+                    </div>
                 </div>
         return(
-            <div>
-
-                <div className={"level"} style={{marginBottom: "0"}}>
-                    <div className={"level-item"}>
-                        Now Playing...
-                    </div>
-                </div>
-                <div className={"level"}>
-                    <div className={"level-item"}>
-                        {content}
-                    </div>
-                </div>
-            </div>
+            <span>
+            {content}
+            </span>
         )
 
 
