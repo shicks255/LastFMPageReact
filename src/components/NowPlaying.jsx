eac9b83dea028deb36a55b16bf26d50b6fd7b6ad
shicks <shicks255@yahoo.com>
2019-04-10 22:19:47 -0400

    changing this back into a functional component

--- src/components/NowPlaying.jsx
+++ src/components/NowPlaying.jsx
@@ -1,58 +1,42 @@
 import React from 'react';
 
 
-export default class NowPlaying extends React.Component {
+export default function NowPlaying(props) {
 
-    constructor(props)
-    {
-        super(props);
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot)
-    {
-        // if (prevProps.nowPlaying !== this.props.nowPlaying)
-        // {
-        //     console.log('update from now playing');
-        //     this.render();
-        // }
-    }
-
-    render(){
-        let nowPlaying;
-        if (this.props.nowPlaying)
-            nowPlaying =
-                <div className={"nowPlaying"}>
-                    <br/>
-                    <div className={"level"}>
-                        <div className={"level-item"} >
-                            <table>
-                                <tbody>
-                                <tr>
-                                    <td rowSpan={2}>
-                                        <img alt={""} className={"image"} src={this.props.nowPlaying.image[1]['#text']}/>
-                                    </td>
-                                    <td>&nbsp;</td>
-                                    <td style={{textAlign: "center"}}>
+    let nowPlaying;
+    if (props.nowPlaying)
+        nowPlaying =
+            <div className={"nowPlaying"}>
+                <br/>
+                <div className={"level"}>
+                    <div className={"level-item"} >
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td rowSpan={2}>
+                                    <img alt={""} className={"image"} src={props.nowPlaying.image[1]['#text']}/>
+                                </td>
+                                <td>&nbsp;</td>
+                                <td style={{textAlign: "center"}}>
                                     <span className={"nowPlaying"}>
-                                        {this.props.nowPlaying.artist['#text']} - {this.props.nowPlaying.name}
+                                        {props.nowPlaying.artist['#text']} - {props.nowPlaying.name}
                                     </span>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>&nbsp;</td>
-                                    <td>
-                                        <img alt={""} className={"gif"} src={process.env.PUBLIC_URL + "/YdBO.gif"}/>
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td>
+                                    <img alt={""} className={"gif"} src={process.env.PUBLIC_URL + "/YdBO.gif"}/>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-        return(
-            <span>
+            </div>
+    return(
+        <span>
             {nowPlaying}
             </span>
-        );
-    }
+    );
 }
