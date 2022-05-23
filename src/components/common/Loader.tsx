eb6a6c1b990ba866692513a1e30fafd3209372fe
shicks <shicks255@yahoo.com>
2022-05-22 19:36:18 -0500

    fixing build

--- src/components/common/Loader.tsx
+++ src/components/common/Loader.tsx
@@ -7,11 +7,11 @@ interface IProps {
 const Loader: React.FC<IProps> = (props: IProps): JSX.Element => {
   const { small } = props;
   const clazz = small ? 'fa-5x' : 'fa-7x';
+  console.log(clazz);
 
   return (
     <div className="text-sky-900">
       <div className="">
-        {/* <i className={`fas fa-compact-disc ${clazz} fa-spin`} /> */}
         <img alt="" className="h-32 w-32 spin" src={`${process.env.PUBLIC_URL}/disc_2.svg`} />
       </div>
     </div>
