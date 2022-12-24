a32a62331c543fc5ab4805f89cf5a79657030e8d
shicks <shicks255@yahoo.com>
2022-12-23 22:13:54 -0600

    removing all GA stuff

--- src/hooks/useAnalytics.tsx
+++ src/hooks/useAnalytics.tsx
@@ -2,33 +2,28 @@ declare global {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   interface Window {
     gtag: (...args: unknown[]) => void;
+    dataLayer: {
+      push: (a: unknown) => void;
+    };
   }
 }
 
 export function sendChangeView(view: string) {
-  window.gtag('event', 'nav_click', {
-    navItem: view
-  });
+  window.dataLayer.push({ event: 'nav_click', navItem: view });
 }
 
 export function sendChangeVisual(visual: string) {
-  window.gtag('event', 'visual_click', {
-    visualItem: visual
-  });
+  window.dataLayer.push({ event: 'visual_click', visualItem: visual });
 }
 
 export function sendChangeUsername() {
-  window.gtag('event', 'username_submit');
+  window.dataLayer.push({ event: 'username_submit' });
 }
 
 export function sendChangeRankStrategy(strategy: string) {
-  window.gtag('event', 'rank_strategy_click', {
-    rankStrategy: strategy
-  });
+  window.dataLayer.push({ event: 'rank_strategy_click', rankStrategy: strategy });
 }
 
 export function sendChangeTimeFrame(timeFrame: string) {
-  window.gtag('event', 'time_frame_click', {
-    timeFrame: timeFrame
-  });
+  window.dataLayer.push({ event: 'time_frame_click', timeFrame: timeFrame });
 }
